73da239553c87d65d966cdb6b459e88dc4dfbb60
BrandonReyes0609 <rey22992@uvg.edu.gt>
2025-03-03 00:19:19 -0600

    Fix problem

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from analyze_complexity import measure_execution_times, plot_execution_times
 
 if __name__ == "__main__":
     print("Ejecutando Máquina de Turing para Fibonacci...")
-    run_fibonacci()
+    run_fibonacci(5, verbose=True)  # Ahora usa n como argumento
 
     print("\nAnalizando tiempos de ejecución...")
     n_values = [2, 3, 4, 5, 6]
